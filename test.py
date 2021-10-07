2fb33172d2f9ac11c39683f99552cc0321ffcced
varunmj <varunmj978@gmail.com>
2021-10-08 00:57:24 +0530

    new version

--- test.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-source = requests.get('https://command-center.support.aws.a2z.com/supportDashboard#/dashboard/?profileName=Linux&supportRegion=NA&supportType=PS')
-print(source.text)
